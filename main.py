f1b1da4824237f143e7d7cb522130a6ba4741046
Sigmanificient <edhyjox@gmail.com>
2021-05-15 00:00:03 +0200

    adding gfxdraw & icon

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 import numba
 import pygame
+from pygame import gfxdraw  # Module not present in pygame default import, why ?
 
 win_size_x = 1024
 win_size_y = 710
@@ -11,6 +12,9 @@ pygame.init()
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((win_size_x, win_size_y))
+
+icon = pygame.image.load("icon.png").convert_alpha()
+pygame.display.set_icon(icon)
 
 
 def main():

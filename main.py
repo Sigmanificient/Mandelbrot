7dd61b7343f557bad027bda0e672bcde04ae471e
Sigmanificient <edhyjox@gmail.com>
2021-05-15 01:54:11 +0200

    Filter order !

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import inspect
+import math
 
 import pygame
 from pygame import gfxdraw  # Module not present in pygame default import, why ?
@@ -19,22 +20,16 @@ pygame.display.set_icon(icon)
 
 class Filter:
 
-    def __init__(self, condition, modifier):
+    def __init__(self, condition, modifier, active=False):
         self.condition = condition
         self.modifier = modifier
-        self.active = True
 
         self.needed_parameters = {
             'condition': tuple(inspect.signature(condition).parameters.keys()),
             'modifier': tuple(inspect.signature(modifier).parameters.keys())
         }
 
-        print(self.needed_parameters)
-
     def apply(self, **kwargs):
-        if not self.active:
-            return kwargs['rgb']
-
         cond_args = [
             kwargs[kwarg] for kwarg in kwargs if kwarg in self.needed_parameters['condition']
         ]
@@ -55,9 +50,13 @@ class App:
         self.is_running = True
         self.show_progress_bar = True
         self.filters = [
-            Filter(lambda i, n: (i / n) % 2, lambda i: (i, i, i)),
-            Filter(lambda i, n: (i / n) == 1, lambda: (0, 0, 0)),
+            Filter(lambda i, n: (i / n) % 2, default_filter, True),
+            # Add your filters here !
+
+            Filter(lambda: True, lambda i, n: ((c := math.sqrt((i / n)) * 255) // 2, c // 2, c))
         ]
+
+        self.active_filters = [0]
 
     def handle_event(self, event):
         if event.type == pygame.QUIT:
@@ -70,6 +69,26 @@ class App:
         if event.key == pygame.K_SPACE:
             self.show_progress_bar = not self.show_progress_bar
 
+        if event.unicode.isdigit():
+            key = int(event.unicode)
+
+            if len(self.filters) <= key:
+                return
+
+            if key in self.active_filters:
+                self.active_filters.remove(key)
+                return
+
+            self.active_filters.append(key)
+
+    def get_color(self, i, n, z):
+        rgb = [0] * 3
+
+        for filter_index in self.active_filters:
+            rgb = self.filters[filter_index].apply(rgb=rgb, i=i, n=n, z=z)
+
+        return rgb
+
     def run(self):
         mandelbrot_points = get_points(100)
         x, y = 0, 0
@@ -80,7 +99,7 @@ class App:
 
             for y in range(win_size_y):
                 x, i, n, z = next(mandelbrot_points)
-                gfxdraw.pixel(screen, x, y, get_color(i, n, z))
+                gfxdraw.pixel(screen, x, y, self.get_color(i, n, z))
 
             if self.show_progress_bar:
                 gfxdraw.line(screen, x + 1, 0, x + 1, win_size_y, RED)
@@ -110,13 +129,9 @@ def get_points(n):
                 yield x, i, n, z
 
 
-def get_color(i, n, z):
-    rgb = [0] * 3
-
-    for _filter in app.filters:
-        rgb = _filter.apply(rgb=rgb, i=i, n=n, z=z)
-
-    return rgb
+def default_filter(i, n):
+    i *= not ((i/n) % 2 == 1)
+    return i, i, i
 
 
 if __name__ == '__main__':

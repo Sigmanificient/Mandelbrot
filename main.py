e24bf60413dc7cc271f9291e4e3713604adf65cb
Sigmanificient <edhyjox@gmail.com>
2021-05-15 00:03:56 +0200

    simple class structure

--- main.py
+++ main.py
@@ -17,17 +17,22 @@ icon = pygame.image.load("icon.png").convert_alpha()
 pygame.display.set_icon(icon)
 
 
-def main():
-    is_running = True
+class App:
 
-    while is_running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                is_running = False
+    def __init__(self):
+        self.is_running = True
 
-        pygame.display.update()
-        clock.tick(-1)
+    def __call__(self):
+        while self.is_running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_running = False
+
+            pygame.display.update()
+            clock.tick(-1)
 
 
 if __name__ == '__main__':
-    main()
+    app = App()
+    app()

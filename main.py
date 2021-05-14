72271c7a6176cd384f712a6d6a6735fbc39f0f5c
Sigmanificient <edhyjox@gmail.com>
2021-05-14 23:49:36 +0200

    simple pygame infinite loop

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+import numba
+import pygame
+
+pygame.init()
+
+time = pygame.time.Clock()
+
+
+def main():
+    is_running = True
+
+    while is_running:
+        pygame.display.update()
+        clock.tick(-1)
+
+
+if __name__ == '__main__':
+    main()
